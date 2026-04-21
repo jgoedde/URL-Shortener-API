e7c4e44e484deb531436d581626f6f5dbc3f3a6a
Julian Gödde <juliangd02@gmail.com>
2026-04-21 19:32:26 +0200

    perf: Add unique index to `ShortCode`

--- src/Infrastructure/Databases/UrlShortener/Configuration/UrlConfiguration.cs
+++ src/Infrastructure/Databases/UrlShortener/Configuration/UrlConfiguration.cs
@@ -1,5 +1,12 @@
 namespace UrlShortener.Infrastructure.Databases.UrlShortener.Configuration;
 
 using Application.Urls.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-internal class UrlConfiguration : EntityConfiguration<Url>;
+internal class UrlConfiguration : EntityConfiguration<Url>
+{
+    public override void Configure(EntityTypeBuilder<Url> builder)
+    {
+        builder.HasIndex(x => x.ShortCode).IsUnique();
+    }
+}
---
+++ src/Infrastructure/Migrations/20260421173217_AddIndexToShortCode.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToShortCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_ShortCode",
+                table: "Urls",
+                column: "ShortCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Urls_ShortCode",
+                table: "Urls");
+        }
+    }
+}
